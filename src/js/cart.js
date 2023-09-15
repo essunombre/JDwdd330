c1af5e0c8846e7347921d659292537fd0f7a2aac
essunombre <alb21001@byui.edu>
2023-09-15 18:24:08 -0500

    fixing to have the items on cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,9 +1,18 @@
 import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
+  const cartItems = new Array(getLocalStorage("so-cart"));
+  console.log("Cart Items:", cartItems); // Add this line for debugging
+  // A check if cartItems array is empty and if it is, return to exit the function
+  if (cartItems.length === 0) {
+    // Display a message or take some action when there are no items
+    const noItemsMessage = "<p>No items in the cart.</p>";
+    document.querySelector(".product-list").innerHTML = noItemsMessage;
+    return; // Exit the function, as there are no items to render
+  }
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  console.log(htmlItems); // Log the contents of htmlItems
 }
 
 function cartItemTemplate(item) {
